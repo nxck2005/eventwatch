18b2d1a0a590f4084b8661737bdb60b745d8ecb6
Nishchal Ravi <nxckmusicprod@gmail.com>
2024-11-12 02:56:44 +0530

    Redirect / of project to /watch app

--- eventwatch/eventwatch/settings.py
+++ eventwatch/eventwatch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-32y-f4ryod6jf56p19(@p@_qzo%95jygvf4*m$=0w!*#(exd+m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
--- eventwatch/eventwatch/urls.py
+++ eventwatch/eventwatch/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
+    path('', lambda request: redirect('watch/', permanent=True)),  # Redirect root URL to /watch/
     path('admin/', admin.site.urls),
     path('watch/', include("watch.urls"))
 ]
